aba744771c91473c0e598b9a9b21c1d3deb687f6
Cronwel Noel Irias <noelirias@gmail.com>
2017-08-11 11:04:50 -0400

    configured initial articles under devise user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:show]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @articles = Article.all.order('created_at DESC')
   end
 
   def new
-    @article = Article.new
+    @article = current_user.articles.build
   end
 
   def show
@@ -13,9 +14,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
     if @article.save
-      redirect_to @article
+      redirect_to @article, notice: 'Successfully created a new article'
     else
       render 'new'
     end
